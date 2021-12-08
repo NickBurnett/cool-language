afc67537e3f06373399b07686257e7b8db9076a4
Nicolas Andrew Burnett <nicolasandrewburnett@gmail.com>
2021-12-08 00:33:17 -0600

    feat(parser): implemented parsing for println and static semantics

--- interpreter/parser.py
+++ interpreter/parser.py
@@ -2,6 +2,31 @@ from interpreter.lexer import OutputType, LexOutput
 
 # UTILITY
 
+class VariableStore:
+  def __init__(self):
+    self.store = list()
+    self.push()
+  
+  def push(self):
+    self.store.append(dict())
+  
+  def pop(self):
+    self.store.pop()
+  
+  def setVar(self, var, val = 0):
+    for i in range(len(self.store) - 1, -1, -1):
+      d = self.store[i]
+      if var in d:
+        d[var] = val
+    self.store[-1][var] = val
+  
+  def getVar(self, var):
+    for i in range(len(self.store) - 1, -1, -1):
+      d = self.store[i]
+      if var in d:
+        return d[var]
+    return None
+
 class ParseOutput:
   def __init__(self, success: bool, data):
     self.success = success
@@ -9,6 +34,7 @@ class ParseOutput:
 
 
 tokens = []
+store: VariableStore
 debugging: bool = False
 
 def debug(t: str, token: LexOutput):
@@ -41,15 +67,27 @@ def parseValue():
     error(token)
     return False
   if token.output_type == OutputType.TOKEN_ID:
-    return True
+    if store.getVar(token.output) == None:
+      error(token)
+      return False
+    return ["value", ["id", token.output]]
   elif token.output_type == OutputType.TOKEN_INT:
-    return True
+    return ["value", ["int", token.output]]
   elif token.output == "-":
-    return parseValue()
+    value = parseValue()
+    if value == False:
+      return False
+    return ["value", "-", value]
   elif token.output == "not":
-    return parseValue()
+    value = parseValue()
+    if value == False:
+      return False
+    return ["value", "not", value]
   elif token.output == "(":
-    return parseExpr() and next().output == ")"
+    expr = parseExpr()
+    if expr == False or next().output != ")":
+      return False
+    return ["value", expr]
   else:
     error(token)
     return False
@@ -60,23 +98,41 @@ def parseVExpr():
   if token.output_type != OutputType.LEXEME:
     revert(token)
     debug("revert", None)
-    return True
+    return ["nop"]
   if token.output == ">":
-    return parseValue()
+    value = parseValue()
+    if value == False:
+      return False
+    return ["v_expr", ">", value]
   elif token.output == ">=":
-    return parseValue()
+    value = parseValue()
+    if value == False:
+      return False
+    return ["v_expr", ">=", value]
   elif token.output == "<":
-    return parseValue()
+    value = parseValue()
+    if value == False:
+      return False
+    return ["v_expr", "<", value]
   elif token.output == "<=":
-    return parseValue()
+    value = parseValue()
+    if value == False:
+      return False
+    return ["v_expr", "<=", value]
   elif token.output == "==":
-    return parseValue()
+    value = parseValue()
+    if value == False:
+      return False
+    return ["v_expr", "==", value]
   elif token.output == "!=":
-    return parseValue()
+    value = parseValue()
+    if value == False:
+      return False
+    return ["v_expr", "!=", value]
   else:
     revert(token)
     debug("revert", None)
-    return True
+    return ["nop"]
 
 def parseFExpr():
   token = next()
@@ -84,21 +140,36 @@ def parseFExpr():
   if token.output_type != OutputType.LEXEME:
     revert(token)
     debug("revert", None)
-    return True
+    return ["nop"]
   if token.output == "*":
-    return parseTerm()
+    term = parseTerm()
+    if term == False:
+      return False
+    return ["f_expr", "*", term]
   elif token.output == "/":
-    return parseTerm()
+    term = parseTerm()
+    if term == False:
+      return False
+    return ["f_expr", "/", term]
   elif token.output == "%":
-    return parseTerm()
+    term = parseTerm()
+    if term == False:
+      return False
+    return ["f_expr", "%", term]
   else:
     revert(token)
     debug("revert", None)
-    return True
+    return ["nop"]
 
 def parseFactor():
   debug("factor", None)
-  return parseValue() and parseVExpr()
+  value = parseValue()
+  if value == False:
+    return False
+  vExpr = parseVExpr()
+  if vExpr == False:
+    return False
+  return ["factor", value, vExpr]
 
 def parseTExpr():
   token = next()
@@ -106,19 +177,31 @@ def parseTExpr():
   if token.output_type != OutputType.LEXEME:
     revert(token)
     debug("revert", None)
-    return True
+    return ["nop"]
   if token.output == "+":
-    return parseNExpr()
+    nExpr = parseNExpr()
+    if nExpr == False:
+      return False
+    return ["t_expr", "+", nExpr]
   elif token.output == "-":
-    return parseNExpr()
+    nExpr = parseNExpr()
+    if nExpr == False:
+      return False
+    return ["t_expr", "-", nExpr]
   else:
     revert(token)
     debug("revert", None)
-    return True
+    return ["nop"]
 
 def parseTerm():
   debug("term", None)
-  return parseFactor() and parseFExpr()
+  factor = parseFactor()
+  if factor == False:
+    return False
+  fExpr = parseFExpr()
+  if fExpr == False:
+    return False
+  return ["term", factor, fExpr]
 
 def parseBExpr():
   token = next()
@@ -126,41 +209,70 @@ def parseBExpr():
   if token.output_type != OutputType.TOKEN_KEYWORD:
     revert(token)
     debug("revert", None)
-    return True
+    return ["nop"]
   if token.output == "and":
-    return parseNExpr()
+    nExpr = parseNExpr()
+    if nExpr == False:
+      return False
+    return ["b_expr", "and", nExpr]
   elif token.output == "or":
-    return parseNExpr()
+    nExpr = parseNExpr()
+    if nExpr == False:
+      return False
+    return ["b_expr", "or", nExpr]
   else:
     revert(token)
     debug("revert", None)
-    return True
+    return ["nop"]
 
 def parseNExpr():
   debug("n_expr", None)
-  return parseTerm() and parseTExpr()
+  term = parseTerm()
+  if term == False:
+    return False
+  tExpr = parseTExpr()
+  if tExpr == False:
+    return False
+  return ["n_expr", term, tExpr]
 
 def parseExpr():
   debug("expression", None)
-  return parseNExpr() and parseBExpr()
+  nExpr = parseNExpr()
+  if nExpr == False:
+    return False
+  bExpr = parseBExpr()
+  if bExpr == False:
+    return False
+  return ["expr", nExpr, bExpr]
 
 def parseExitArg():
-  return parseExpr()
+  expr = parseExpr()
+  if expr == False:
+    return False
+  return expr
 
 def parseShiftArg():
   token = next()
-  if token.output_type != OutputType.TOKEN_ID:
+  if token.output_type != OutputType.TOKEN_ID or store.getVar(token.output) == None:
     error(token)
     return False
-  return next().output == "," and parseExpr()
+  if next().output != ",":
+    return False
+  expr = parseExpr()
+  if expr == False:
+    return False
+  return [token.output, expr]
 
 def parsePrintArg():
   token = next()
   debug("print", token)
   if token.output_type == OutputType.TOKEN_STRING:
-    return True
+    return ["string", token.output]
   revert(token)
-  return parseExpr()
+  expr = parseExpr()
+  if expr == False:
+    return False
+  return expr
 
 def parseStmt():
   token = next()
@@ -171,27 +283,69 @@ def parseStmt():
   if token.output == "else":
     revert(token)
     debug("revert", None)
-    return True
+    return ["nop"]
   elif token.output == "end":
     revert(token)
     debug("revert", None)
-    return True
+    return ["nop"]
   elif token.output == "print":
-    return parsePrintArg()
+    printCmd = parsePrintArg()
+    if printCmd == False:
+      return False
+    return ["print", printCmd]
+  elif token.output == "println":
+    printCmd = parsePrintArg()
+    if printCmd == False:
+      return False
+    return ["println", printCmd]
   elif token.output == "shift":
-    return parseShiftArg()
+    shiftCmd = parseShiftArg()
+    if shiftCmd == False:
+      return False
+    return ["shift"] + shiftCmd
   elif token.output == "exit":
-    return parseExitArg()
+    exitCmd = parseExitArg()
+    if exitCmd == False:
+      return False
+    return ["exit", exitCmd]
   elif token.output == "get":
     token = next()
-    return token.output_type == OutputType.TOKEN_ID
+    if token.output_type != OutputType.TOKEN_ID:
+      return False
+    store.setVar(token.output)
+    return ["get", token.output]
   elif token.output_type == OutputType.TOKEN_ID:
+    oldToken = token
     token = next()
-    return token.output == "=" and parseExpr()
+    if token.output != "=":
+      return False
+    expr = parseExpr()
+    if expr == False:
+      return False
+    store.setVar(oldToken.output)
+    return ["set", oldToken.output, expr]
   elif token.output == "if":
-    return parseExpr() and next().output == "then" and parseStmtList() and next().output == "else" and parseStmtList() and next().output == "end"
+    store.push()
+    expr = parseExpr()
+    if expr == False or next().output != "then":
+      return False
+    thenList = parseStmtList()
+    if thenList == False or next().output != "else":
+      return False
+    store.push()
+    elseList = parseStmtList()
+    if elseList == False or next().output != "end":
+      return False
+    return ["if", expr, thenList, elseList]
   elif token.output == "while":
-    return parseExpr() and next().output == "do" and parseStmtList() and next().output == "end"
+    store.push()
+    expr = parseExpr()
+    if expr == False or next().output != "do":
+      return False
+    stmtList = parseStmtList()
+    if stmtList == False or next().output != "end":
+      return False
+    return ["while", expr, stmtList]
   else:
     error(token)
     return False
@@ -200,9 +354,11 @@ def parseStmtList():
   token = next()
   debug("stmt_list", token)
   if token.output_type == OutputType.NONE:
-    return True
+    return [["nop"]]
   revert(token)
   stmt = parseStmt()
+  if stmt == False:
+    return False
   token = next()
   if token.output != ";":
     error(token)
@@ -210,16 +366,27 @@ def parseStmtList():
   token = next()
   if token.output == "else" or token.output == "end":
     revert(token)
-    return stmt
+    store.pop()
+    return [stmt]
   else:
     revert(token)
-  return stmt and parseStmtList()
+  stmtList = parseStmtList()
+  if stmtList == False:
+    return False
+  return [stmt] + stmtList
 
-def parseProgram(lexed_tokens, debug = False):
+def parseProgram(lexed_tokens, varStore, debug = False):
   global tokens
+  global store
   global debugging
+  store = varStore
   debugging = debug
   if len(lexed_tokens) <= 0:
     return False
   tokens = lexed_tokens
-  return parseStmtList()
+  store.push()
+  stmtList = parseStmtList()
+  if stmtList == False:
+    return False
+  store.pop()
+  return stmtList
